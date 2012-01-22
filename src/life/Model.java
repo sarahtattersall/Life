13753ab24a71dcc1c00eff732c488b651149a0c8
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-01-22 19:10:54 +0000

    Added better comment in Model about takeTurn()

--- src/life/Model.java
+++ src/life/Model.java
@@ -15,7 +15,8 @@ public class Model {
 	// If a new cell is born it inherits the majority color of it's surrounding cells.	
 	// Here it is simpler to have the cell contain it's next state and loop through 
 	// updating the next state and then commit all the states at the end rather than
-	// having a new grid. The grid iterator calculates the neighbour count and majority color.
+	// having a new grid. It is also more efficient as there is not duplicate data.
+	// The grid iterator calculates the neighbour count and majority color.
 	public void takeTurn(){
 		for( EdgelessGrid.GridIterator itr = grid.iterator(); itr.hasNext();){
 			LifeCell cell = itr.next();
